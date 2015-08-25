e16c1dff28c886c883e4f64bdcf0008b3b607f5c
Erik Madsen <erik@madsense.net>
2015-08-24 20:09:45 -0400

    some test views

--- sdss/urls.py
+++ sdss/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^api/get_songs', views.get_songs, name='get_songs'),
 ]
--- sdss/views.py
+++ sdss/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import JsonResponse
+from .custom_json import CustomJSONEncoder
 
-from .models import Release
+from .models import Track
 
 class IndexView(generic.ListView):
     template_name = 'sdss/index.html'
@@ -16,3 +18,18 @@ class IndexView(generic.ListView):
         #     pub_date__lte=timezone.now()
         # ).order_by('-pub_date')[:5]
         return Release.objects.filter(album_type=1)
+
+def get_songs(request):
+    #songs = Song.objects.all().values('id', 'title', 'writers', 'originally_by', 'is_single')
+    tracks = Track.objects.filter(release__album_type=1) \
+      .values('song__id', 'song__title', 'song__is_single',
+              'song__originally_by', 'release__release_date',
+              'release__title', 'duration')
+    return JsonResponse(list(tracks), encoder=CustomJSONEncoder, safe=False)
+
+# def get_releases(request):
+#     releases = list(Release.objects.all().values('id','title','release_date'))
+#     for release in releases:
+#         release['tracklist'] = list(Track.objects.filter(release__id=release['id'])\
+#           .values('track_number','song__title','get_duration_seconds'))
+#     return JsonResponse(releases, safe=False)
