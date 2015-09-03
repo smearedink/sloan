ce1a0c82d0e9fd7c158848fe92780d84ab15011e
Erik Madsen <erik@madsense.net>
2015-09-03 09:58:48 -0400

    new migrations

--- sdss/migrations/0001_initial.py
+++ sdss/migrations/0001_initial.py
@@ -25,34 +25,36 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=80)),
-                ('album_type', models.CharField(max_length=20)),
+                ('album_type', models.PositiveSmallIntegerField(default=0, choices=[(1, b'LP'), (2, b'EP'), (3, b'single'), (4, b'compilation'), (5, b'live'), (0, b'other')])),
                 ('release_date', models.DateField()),
             ],
+            options={
+                'ordering': ['release_date'],
+            },
         ),
         migrations.CreateModel(
             name='Song',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=80)),
-                ('version_info', models.CharField(default=b'', max_length=80, blank=True)),
-                ('is_single', models.BooleanField(default=False)),
-                ('duration', models.FloatField(default=0)),
-                ('bpm', models.PositiveSmallIntegerField(default=0)),
-                ('key', models.PositiveSmallIntegerField(blank=True, null=True, choices=[(0, b'C'), (1, b'C sharp'), (2, b'D'), (3, b'E flat'), (4, b'E'), (5, b'F'), (6, b'F sharp'), (7, b'G'), (8, b'A flat'), (9, b'A'), (10, b'B flat'), (11, b'B')])),
-                ('mode', models.PositiveSmallIntegerField(blank=True, null=True, choices=[(0, b'minor'), (1, b'major')])),
-                ('version_of', models.ForeignKey(to='sdss.Song', null=True)),
-                ('writers', models.ManyToManyField(to='sdss.BandMember', blank=True)),
+                ('is_single', models.BooleanField(default=False, verbose_name=b'Single?')),
+                ('originally_by', models.CharField(default=b'', help_text=b'Name of the original band or musician if this song is a cover, otherwise blank', max_length=80, blank=True)),
+                ('writers', models.ManyToManyField(help_text=b'Select none if this is a cover song', to='sdss.BandMember', blank=True)),
             ],
         ),
         migrations.CreateModel(
             name='Track',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('release_version', models.CharField(default=b'canonical', max_length=80)),
                 ('track_number', models.PositiveSmallIntegerField()),
+                ('version_info', models.CharField(default=b'', help_text=b'This is the text in parentheses after a song name', max_length=80, blank=True)),
+                ('duration', models.DurationField(default=0)),
                 ('release', models.ForeignKey(to='sdss.Release')),
                 ('song', models.ForeignKey(to='sdss.Song')),
             ],
+            options={
+                'ordering': ['track_number'],
+            },
         ),
         migrations.AddField(
             model_name='release',
