e36a96b630611946322651ec3a6da88bf96d8183
Erik Madsen <erik@madsense.net>
2015-09-03 09:48:26 -0400

    changed to postgres db for Heroku

--- sloan/settings.py
+++ sloan/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'sloan.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+#    'old': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sloan',
+        'USER': 'madsense',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
