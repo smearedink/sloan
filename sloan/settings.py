18147703626bd695949e38f100a0401e062ba694
Erik Madsen <erik@madsense.net>
2015-09-08 20:56:53 -0400

    use postgres db locally or on heroku

--- sloan/settings.py
+++ sloan/settings.py
@@ -80,14 +80,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-##    'default': {
-##        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-##        'NAME': 'sloan',
-##        'USER': 'madsense',
-##        'PASSWORD': '',
-##        'HOST': '',
-##        'PORT': '5432',
-##    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sloan',
+        'USER': 'madsense',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
+    }
 }
 
 
@@ -111,4 +111,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+dbconfig = dj_database_url.config()
+if len(dbconfig):
+    DATABASES['default'] = dj_database_url.config()
